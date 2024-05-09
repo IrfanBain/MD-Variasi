6b81daa95c415187bf5bbeef4a45720ce94874e5
IrfanBain <irfannurbain@gmail.com>
2024-05-09 10:53:39 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./component/element/navbar"
+import Mainbar from "./component/element/mainbar"
+function App() {
+
+
+  return (
+    <div>
+      <Navbar /> 
+      <Mainbar /> 
+      <h1 className='text-3xl font-bold text-center'></h1>
+    </div>
+  )
+}
+
+export default App
